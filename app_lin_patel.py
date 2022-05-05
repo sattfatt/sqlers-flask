41528a731887f3ce8fb2046b46331a5df38440b2
Satyam Patel <patelsat@flip2.engr.oregonstate.edu>
2022-05-04 18:06:05 -0700

    Made pages even easier to generate

--- app_lin_patel.py
+++ app_lin_patel.py
@@ -13,13 +13,17 @@ def hello_world():
 @app.route("/customers")
 def customers():
     return render_template(
-        'customers.html',
+        'entity.html',
         title="Customers",
         headers=["customer_id", "first_name", "last_name", "age", "email", "is_active", "street", "city", "state", "zip", "country", "phone"],
         data=[
             [1,"bob", "patel", "13", "asdf@sadfa.com", "true", "asdf N street", "porterville", "california", "54453", "US", "123123211"],
             [1,"bob", "patel", "13", "asdf@sadfa.com", "true", "asdf N street", "porterville", "california", "54453", "US", "123123211"]
-        ]
+        ],
+        PageHeader="Customers Table",
+        UpdateRoute="/customers", #TODO change this in the future to the actual route
+        DeleteRoute="/customers", #TODO change this in the future to the actual route
+        InsertRoute="/customers" #TODO change this in the future to the actual route
     )
 
 if __name__ == "__main__":
