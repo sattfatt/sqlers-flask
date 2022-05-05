cf482e3367fbc1e3f13d9d55baacaf4d6e5e91c0
Satyam Patel <patelsat@flip2.engr.oregonstate.edu>
2022-05-04 17:05:49 -0700

    Created the basic structure to get started with our tables

--- app_lin_patel.py
+++ app_lin_patel.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p> Hello </p>"
+    return render_template(
+        'home.html',
+        title="Home Page"
+    )
+
+@app.route("/customers")
+def customers():
+    return render_template(
+        'customers.html',
+        title="Customers",
+        headers=["customer_id", "first_name", "last_name", "age", "email", "is_active", "street", "city", "state", "zip", "country", "phone"],
+        data=[
+            [1,"bob", "patel", "13", "asdf@sadfa.com", "true", "asdf N street", "porterville", "california", "54453", "US", "123123211"],
+            [1,"bob", "patel", "13", "asdf@sadfa.com", "true", "asdf N street", "porterville", "california", "54453", "US", "123123211"]
+        ]
+    )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8765, debug=False)
+    app.run(host="0.0.0.0", port=8765, debug=True)
